06175903a77d4689f70ef78986b533482510043d
Sales Arofat <96236114+rofat8899@users.noreply.github.com>
2022-01-21 15:33:21 +0700

    update

--- src/main/java/com/rofat/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/rofat/coronavirustracker/controllers/HomeController.java
@@ -3,8 +3,14 @@ package com.rofat.coronavirustracker.controllers;
 import com.rofat.coronavirustracker.dto.CovidDTO;
 import com.rofat.coronavirustracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RestController
 public class HomeController {
@@ -12,8 +18,20 @@ public class HomeController {
     @Autowired
     private CoronaVirusDataService coronaVirusDataService;
 
+    @CrossOrigin("http://localhost:9000")
+    @ResponseBody
     @GetMapping("/api/covid")
-    public CovidDTO getNewCases() {
-        return coronaVirusDataService.getNewCases();
+    public ResponseEntity<CovidDTO>  getNewCases()  {
+        try {
+            CovidDTO covid = coronaVirusDataService.getNewCases();
+            return new ResponseEntity<>(covid, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/api/message")
+    public String getMessage(){
+        return "hello world";
     }
 }
